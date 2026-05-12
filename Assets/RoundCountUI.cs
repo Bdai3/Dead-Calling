377adb6d45fc4cd73281177d96fac067562c5df1
Bdai3 <daibj@uci.edu>
2026-05-12 15:34:54 -0700

    Added Round Count UI and increased size of points and ammo UI

--- Assets/RoundCountUI.cs
+++ Assets/RoundCountUI.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class RoundCountUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public static RoundCountUI Instance;
+    public TextMeshProUGUI roundText;
 
     // Update is called once per frame
     void Update()
     {
-
+        int round = Variables.Scene(gameObject.scene).Get<int>("Round");
+        roundText.text = round.ToString();
     }
 }
